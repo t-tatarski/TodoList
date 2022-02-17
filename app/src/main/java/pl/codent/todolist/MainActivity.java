3fc20321932e7b5fe379d43dbf6ecc5cb313c608
zomal <tehne@tlen.pl>
2022-02-17 22:45:02 +0100

    Initial commit

---
+++ app/src/main/java/pl/codent/todolist/MainActivity.java
@@ -0,0 +1,27 @@
+package pl.codent.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Activity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+    
+}
