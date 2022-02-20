95858534e1bec0e87f3449a5f0b920da5622ae1b
zomal <tehne@tlen.pl>
2022-02-20 10:20:41 +0100

    commit with recycler

--- app/src/main/java/pl/codent/todolist/MainActivity.java
+++ app/src/main/java/pl/codent/todolist/MainActivity.java
@@ -3,8 +3,10 @@ package pl.codent.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -14,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean firstRunApp = preferences.getBoolean("firstrun",true);
+        if (!firstRunApp) {showToast("witaj pierwszy raz");
+        }else{
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("firstrun",true);
+        editor.commit();
+        showToast("witaj nie pierwszy raz");
+        }
+
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
+
+    private void showToast(String msgText) {
+        Toast.makeText(getApplicationContext(),msgText,Toast.LENGTH_LONG).show();
+    }
+
 }
